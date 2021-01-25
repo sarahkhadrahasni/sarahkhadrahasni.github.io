9b0dd7e094d85a52ecae916b71eb620bfe0b89a7
sarahkhadrahasni <77926885+sarahkhadrahasni@users.noreply.github.com>
2021-01-25 16:51:22 +0100

    Add files via upload

---
+++ sketch18.js
@@ -0,0 +1,82 @@
+let airplane
+var font;
+var vehicles = [];
+let angle = 0;
+
+
+function preload() {
+  font = loadFont('AvenirNextLTPro-Demi.otf');
+  airplane = loadImage ('airplanehome.png');
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  // textFont(font);
+  // textSize(192);
+  // fill(255);
+  // noStroke();
+  // text('train', 100, 200);
+
+  var points = font.textToPoints('Bye Berlin!!', 370, 300, 100, {
+    sampleFactor: 0.25
+  });
+  
+  
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+
+  }
+}
+
+function draw() {
+  background(255, windowWidth, windowHeight);
+  stroke('#0f0');
+  line(pmouseX, pmouseY, mouseX, mouseY);
+  
+        if (mouseIsPressed)
+{
+    noStroke();
+    fill('magenta');
+    text("(time to go home...)", 690, 190);
+    textAlign(CENTER);
+  
+}
+      if (keyIsPressed)
+    {
+  noStroke();
+  fill('magenta');
+  textAlign(CENTER);
+
+  text("I (went to) Berlin (and it was all online)", 910, 350);
+  text("(thank u for joining along the journey :-))", 910, 450);
+
+    }
+     //text(mouseX + "," + mouseY, 20, 20);
+      var imgx = mouseX;
+      var imgy = pmouseY;
+      var angle = atan2(mouseY - imgy, mouseX - imgx);
+        push();
+       // translate(imgx, imgy);
+        rotate(angle);
+        imageMode(CENTER);
+        image(airplane, mouseX, mouseY, 100, 100)
+        pop();
+
+  
+  for (var i = 0; i < vehicles.length; i++) {
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+    
+  }
+  
+
+}
+
+
